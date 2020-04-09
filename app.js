483d01b3bd805da3c2c2555ade9b8ba343d33ebe
Keivin <keivinbr13@gmail.com>
2020-04-08 19:37:22 -0500

    mnsaje oculto

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hola Mundo</h1>');
+  res.send('<div>HcUy6Re2LLBRtj</div>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
