052f92edf79d28f7c023398fd581b1737d63610b
Keivin <keivinbr13@gmail.com>
2020-04-08 20:40:45 -0500

    req query

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>HcUy6Re2LLBRtj</h1>');
+
+    if(req.query.nombre == null || req.query.nombre === ""){
+        res.send('<h1>Hola desconocido </h1>');
+    }else{
+        res.send('<h1>Hola ' + req.query.nombre +  '</h1>');
+    }
+    
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
