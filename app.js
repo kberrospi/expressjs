9efcb91ad472594a7fadbb68edc64324d75f1e91
Keivin <keivinbr13@gmail.com>
2020-04-13 18:55:23 -0500

    Saludame 2 C

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/makers/:name', (req, res) => {
 
-        res.send('<h1>Hola ' + req.param.name  + '!</h1>');
+        res.send('<h1>Hola ' + req.params.name  + '!</h1>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
