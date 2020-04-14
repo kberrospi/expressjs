b8d2fca621360788dc4818ecfb695d1dae9567f1
Keivin <keivinbr13@gmail.com>
2020-04-13 19:01:35 -0500

    Saludame 2 c_1

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express();
 
 app.get('/makers/:name', (req, res) => {
 
-        res.send('<h1>Hola ' + req.params.name  + '!</h1>');
+        function capitalizar(string) 
+        {
+            return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+        }
+
+        let nombre = capitalizar(req.params.name);
+        res.send('<h1>Hola ' +  nombre + '!</h1>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
