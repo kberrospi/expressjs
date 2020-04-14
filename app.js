ba7298728320fe81377bc7d3ff5d423830b6b7f3
Keivin <keivinbr13@gmail.com>
2020-04-13 20:30:32 -0500

    Pares e Impares

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.get('/makers/:name', (req, res) => {
+app.get('/', (req, res) => {
+    let i ; let str ="";
+    for (i=1; i<=50; i++) {
 
-        function capitalizar(string) 
+        if( (i%2) === 0){
+           str+= "<p> " + i + " Soy Par!" + "</p>";
+        }else{
+            str+= "<p> " + i + " Soy Impar!" + "</p>";
+        }
+        
+    }    
+    
+    res.send(str);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
+
+/* 
+
+function capitalizar(string) 
         {
             return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
         }
 
-        let nombre = capitalizar(req.params.name);
-        res.send('<h1>Hola ' +  nombre + '!</h1>');
-});
+        res.send(` <p>${i} Soy Par!</p>`);
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+*/
