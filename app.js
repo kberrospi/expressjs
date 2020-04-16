ff4dd1be69479bcfa6719876f263448dfea6b7a8
kberrospi <keivinbr13@gmail.com>
2020-04-16 18:54:50 -0500

    Visitantes

--- app.js
+++ app.js
@@ -1,36 +1,75 @@
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended: true}));
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { 
+  useNewUrlParser: true 
+});
+
+mongoose.connection.on("error", function(e) { 
+  console.error(e); 
+});
+
+const PersonSchema = mongoose.Schema({
+  name: String,
+  date: Date,
+});
+
+const Visitor = mongoose.model("Persons",PersonSchema);
 
 app.get('/', (req, res) => {
 
-    let html = '<form action="/" method="POST">'+
-    '<div>'+ '<input type="text" id="cnombre" name="nombre">'+ '<button type="submit">Enviar</button>'+ '</div>'+
-    '</form>';
-			  
-    res.send(html);
+    let name = req.query.name;
+    date = Date.now();
    
+    
+
+    if (!name) {
+      name = 'Anónimo';
+      
+      const person = new Visitor({
+        name,
+        date,
+      });
+
+      person.save((error)=>{
+        if (error) {
+          return res.send('Error: ', error);
+        }
+  
+        return res.send('<h1>El visitante fue almacenado con exito.</h1>')
+  
+      });
+    }else{
+
+      const person = new Visitor({
+        name,
+      });
+  
+      person.save((error)=>{
+        if (error) {
+          return res.send('Error: ', error);
+        }
+  
+        return res.send('<h1>El visitante fue almacenado con exito.</h1>')
+  
+      });
+    }
+
+   
+
+    
 });
-
-app.post('/', function(req, res){
-    let name = req.body.nombre;
-    let html = '<h1>Hola ' + name + '!</h1>';
-               
-    res.send(html);
-  });
-
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
 
 
 /* 
 
-function capitalizar(string) 
-        {
+function capitalizar(string) {
             return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
         }
 
-        res.send(` <p>${i} Soy Par!</p>`);
+        
 
 */
